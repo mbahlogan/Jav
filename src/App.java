76d78ef3774ebd74d5e2efe1a27418a9e17f44e4
Mbah Logan <ganlo7740@gmail.com>
2024-01-10 11:48:08 +0100

    GUI box

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import javax.swing.JFrame;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Hello world");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 250);
+        frame.setVisible(true);
     }
 }
